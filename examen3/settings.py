d2d5cf32c5b2b90af46d888fedc87f4fd10b889d
Jairo <jairo234cd@gmail.com>
2025-11-04 12:06:50 -0500

    fix: allowed hosts syntax

--- examen3/settings.py
+++ examen3/settings.py
@@ -148,7 +148,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # CORS CONFIGURATION
 # =======================
 
-CORS_ALLOW_ALL_ORIGINS = DEBUG
+CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
