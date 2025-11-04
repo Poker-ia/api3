750cf9a2d0d65558c65d0e62aee924c4e9256f4a
Jairo <jairo234cd@gmail.com>
2025-11-04 11:31:52 -0500

    fix: allowed hosts syntax

--- api3/models.py
+++ api3/models.py
@@ -10,7 +10,7 @@ class Proveedor(models.Model):
 class Producto(models.Model):
     nombre = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
-    precio = models.DecimalField(max_digits=9000, decimal_places=2)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField()
     imagen = models.ImageField(upload_to='productos/')
     proveedor_id = models.ForeignKey(Proveedor, on_delete=models.CASCADE)   
